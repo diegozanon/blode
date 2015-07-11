4223591c2e0257b8409fe49a5d68978ea73a5d0d
zanon-io <zanon-io@users.noreply.github.com>
2015-07-11 12:32:09 -0300

    Using ngRoute to redirect to posts

--- template/js/routes.js
+++ template/js/routes.js
@@ -1,6 +1,8 @@
 angular.module('zanonApp').config(['$routeProvider', '$locationProvider',
   function($routeProvider, $locationProvider) {
     $routeProvider.
+      
+      // partials
       when('/', {
         templateUrl: 'partials/posts.html',
         controller: 'PostsController'
@@ -8,6 +10,25 @@ angular.module('zanonApp').config(['$routeProvider', '$locationProvider',
       when('/404', {
         templateUrl: 'partials/404.html'
       }).		  
+
+      // posts
+      when('/angular-single-page-application-and-seo', {
+        templateUrl: 'posts/2015-07-05-angular-single-page-application-and-seo.html'
+      }).
+      when('/a-simple-example-of-angular-directives', {
+        templateUrl: 'posts/2015-06-02-a-simple-example-of-angular-directives.html'
+      }).
+      when('/retrieving-data-from-mongodb-using-node', {
+        templateUrl: 'posts/2015-04-07-retrieving-data-from-mongodb-using-node.html'
+      }).
+      when('/an-intro-to-nwjs-forme-node-webkit', {
+        templateUrl: 'posts/2015-03-14-an-intro-to-nwjs-forme-node-webkit.html'
+      }).
+      when('/serving-gzipped-files-in-amazon-cloudfront', {
+        templateUrl: 'posts/2015-02-18-serving-gzipped-files-in-amazon-cloudfront.html'
+      }).
+
+      // 404
       otherwise({
         redirectTo: '/404'
       });

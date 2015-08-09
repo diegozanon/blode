ab32e6c3433c5dd859b6ad6a424ef708c978c04d
zanon-io <zanon-io@users.noreply.github.com>
2015-08-09 03:14:48 -0300

    Removing hashs (#) from URLs

--- template/js/routes.js
+++ template/js/routes.js
@@ -1,37 +1,27 @@
 angular.module('zanonApp').config(['$routeProvider', '$locationProvider',
   function($routeProvider, $locationProvider) {
-    $routeProvider.
+    $routeProvider
       
       // partials
-      when('/', {
+      .when('/', {
         templateUrl: 'partials/posts.html',
         controller: 'PostsController'
-      }).	
-      when('/404', {
+      })	
+      .when('/404', {
         templateUrl: 'partials/404.html',
         controller: '404Controller'
-      }).		  
+      })		  
 
       // posts
-      when('/angular-single-page-application-and-seo', {
-        templateUrl: 'posts/2015-07-05-angular-single-page-application-and-seo.html'
-      }).
-      when('/angular-directives-a-direct-and-simple-guide', {
-        templateUrl: 'posts/2015-06-02-angular-directives-a-direct-and-simple-guide.html'
-      }).
-      when('/retrieving-data-from-mongodb-using-node', {
-        templateUrl: 'posts/2015-04-07-retrieving-data-from-mongodb-using-node.html'
-      }).
-      when('/an-intro-to-nwjs-forme-node-webkit', {
-        templateUrl: 'posts/2015-03-14-an-intro-to-nwjs-forme-node-webkit.html'
-      }).
-      when('/serving-gzipped-files-in-amazon-cloudfront', {
-        templateUrl: 'posts/2015-02-18-serving-gzipped-files-in-amazon-cloudfront.html'
-      }).
+      .when('/angularjs-with-pretty-urls-removing-the-in-amazon-s3', {
+        templateUrl: 'posts/2015-08-09-angularjs-with-pretty-urls-removing-the-in-amazon-s3.html'
+      })      
 
       // 404
-      otherwise({
+      .otherwise({
         redirectTo: '/404'
       });
+
+      $locationProvider.html5Mode(true);
   }
 ]);

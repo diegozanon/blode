4026de8a548d0c138debcf26bece55a72d4fe8ea
zanon-io <zanon-io@users.noreply.github.com>
2016-04-22 18:46:25 -0300

    Created quick starter.

--- src/lib/quick-starter.js
+++ src/lib/quick-starter.js
@@ -1,4 +1,13 @@
+var ncp = require('ncp').ncp;
+var path = require('path');
+
 exports.create = function(name, callback) {
 
+  var source = path.join(__dirname, '../sample');
+  var destination = path.join(path.resolve("."), name);
 
+  // copy folder recursively
+  ncp(source, destination, function (err) {
+    callback(err);
+  });
 }
